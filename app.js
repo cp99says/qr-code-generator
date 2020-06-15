ab9b55a15052dd47714cc59a06c22e591ab1dd69
cp99says <pareekchetan7891@gmail.com>
2020-06-15 15:44:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+  const qr=require('qrcode')
+  const xlsx=require('xlsx')
+
+
+const createQR=(data,filename)=>{
+  qr.toFile("images/"+ filename +".png",data,{width:250,height:250})
+}
+  
+
+
+
+
+const ws=xlsx.readFile("nodeqr.xlsx").Sheets["Sheet1"]
+const data=xlsx.utils.sheet_to_json(ws)
+data.forEach((r)=>{
+  createQR(r.participants,r.filename)
+})
+  
